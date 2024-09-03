0f0a32aeb11fb6e6f65769cce1fe4374ef8aa946
supu <zmaee90909@yahoo.co.jp>
2024-09-04 04:52:06 +0900

    java

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,17 @@
+// var, let, const
+let name = 'Tom';
+name = 'Tim';
+console.log('hello' +name)
+
+Number, string, Boolean, undefined, Null, Symbol
+
+// 動的型付け言語
+let variable = 'str'
+variavle = 12;
+variavle = false;
+variavle = undefined;
+
+console.log(typeof variable)
+
+// 静的型付け言語
+int num = 12;
