33ad0eea0f1ecd37378edfcb6d5ab6fe518501c0
supu <zmaee90909@yahoo.co.jp>
2024-09-05 08:49:11 +0900

    java

--- 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -3,8 +3,6 @@ const hello =function hello(name) {
     console.log('hello ' + name + age);
     return name + age;
 }
-
-
 hello('Code Mafia', 10)
 const returnVal = hello('Code Mafia 2', 20);
 crossOriginIsolated.log(returnVal)
