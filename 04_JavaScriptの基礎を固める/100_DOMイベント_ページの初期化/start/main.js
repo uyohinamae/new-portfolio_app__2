84f5b8c06694fef9147d7c7bdb8a34a7882365cc
supu <zmaee90909@yahoo.co.jp>
2024-09-07 14:04:11 +0900

    dom

--- 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
@@ -2,9 +2,11 @@ const dcl = document.querySelector('.dcl');
 const load = document.querySelector('.load');
 
 document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
+   const h1 = document.querySelector('h1');
+   h1.style.color = 'red';
 });
 
 window.addEventListener("load", function () {
-    load.classList.add('done');
+    const h1 = document.querySelector('h1');
+    h1.style.color = 'red';
 });
