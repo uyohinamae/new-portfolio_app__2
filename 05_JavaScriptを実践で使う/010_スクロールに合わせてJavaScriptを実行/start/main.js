ae40cbe3530d6f9361fac86e197ef331b0372f85
supu <zmaee90909@yahoo.co.jp>
2024-09-11 16:44:49 +0900

    scrole

--- 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -1,12 +1,24 @@
 const child = document.querySelector('.child');
 const cb = function(entries, observer) {
+       entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            console.log('inview');
+            entry.terget.classList.add('inview')
+            // observer.unobserve(entry.target);
+        } else {
+            console.log('out vieww')
+            entry.terget.classList.add('inview')
+        }
+       });
 
-
-    // alert('intersecting')
-
-
+    // alert('intersecting');
+const options = {
+    root: null,
+    rootMargin: "-100px 0px ",
+    threshold: [0, 0.5, 1]
+};
 }
-const io = new IntersectionObserver(cb);
+const io = new IntersectionObserver(cb, options);
 io.observe(child);
-io.observe(child);
-io.observe(child);
+// io.observe(child1);
+// io.observe(child2);
