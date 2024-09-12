4881955f0dda6206e9c237c5a8383157f378a2b3
supu <zmaee90909@yahoo.co.jp>
2024-09-12 18:39:16 +0900

    java

--- 05_JavaScriptを実践で使う/030_スクロール監視クラスの作成/start/main.js
+++ 05_JavaScriptを実践で使う/030_スクロール監視クラスの作成/start/main.js
@@ -1,50 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    
-    // const cb = function (entries, observer) {
-    //     entries.forEach(entry => {
-    //         if (entry.isIntersecting) {
-    //             // const ta = new TextAnimation(entry.target);
-    //             // ta.animate();
-    //             observer.unobserve(entry.target);
-    //         } else {
-    //         }
-    //     });
-    // };
-    // const options = {
-    //     root: null,
-    //     rootMargin: "0px",
-    //     threshold: 0
-    // };
-   
-});
 
-class ScrollObserver {
-    constructor(els, cb, options) {
-        this.els = document.querySelectorAll(els);
-        const defaultptions  = {
-            root: null,
-            rootMargin: "0px",
-            threshold: 0
-        };
-        this.cb = cb;
-        this.options = Object.assign(defaultOptions, options);
-        this._init();
+    const cb = function (el, isIntersecting) {
+        if (isIntersecting) {
+            const ta = new TextAnimation(el);
+            ta.animate();
+        }
     }
-    _init() {
-    const callback = function (entries, observer) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // const ta = new TextAnimation(entry.target);
-                // ta.animate();
-
-                this.cb(entry.target);
-                observer.unobserve(entry.target);
-            } else {
-            }
-        });
-    };
-    const io = new IntersectionObserver(callback, this.options);
-    els.forEach(el => io.observe(el));
-  }
-}   
+    const so = new ScrollObserver('.animate-title', cb);
+});
