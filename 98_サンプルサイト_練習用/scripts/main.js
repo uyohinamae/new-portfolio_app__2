6f5f48b737d2b6c3b27ae71c43c37119ef770cb0
supu <zmaee90909@yahoo.co.jp>
2024-09-14 14:30:14 +0900

    java

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded' , function() {
     const hero = new HeroSlider('.swiper');
     hero.start()
+
+
+    const ta = new TweenTextAnimation('.tween-animate-title')
+    ta.animate();
 });
