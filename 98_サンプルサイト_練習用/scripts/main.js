97b8781118482818ceab619e056f9954fdfb15b1
supu <zmaee90909@yahoo.co.jp>
2024-09-14 15:15:02 +0900

    java

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -3,12 +3,22 @@ document.addEventListener('DOMContentLoaded' , function() {
     hero.start()
 
 
-    const cb = function (el, isIntersecting)  {
+    const _textAnimation = function (el, isIntersecting)  {
         if(isIntersecting)  {
             const ta = new TweenTextAnimation(el);
             ta.animate();
         }
     }
 
-    const so = new ScrollObserver('.tween-animate-title', cb);
+    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+
+    const _inviewAnimation = function (el, inview)  {
+        if(inview)  {
+            el.classList.add('inview');
+        } else {
+            el.classList.remove('inview');
+        }
+    }
+
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
