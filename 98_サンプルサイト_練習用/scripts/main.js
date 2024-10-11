cf98f365b7f82e52629f531ec75f718e47013b46
supu <zmaee90909@yahoo.co.jp>
2024-10-11 18:44:33 +0900

    java

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,56 +1,44 @@
 document.addEventListener('DOMContentLoaded' , function() {
-    const hero = new HeroSlider('.swiper');
-    hero.start()
-
-
-    const _textAnimation = function (el, isIntersecting)  {
-        if(isIntersecting)  {
-            const ta = new TweenTextAnimation(el);
-            ta.animate();
-        }
-    }
-
-    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
-
-    // const _inviewAnimation = function (el, inview)  {
-    //     if(inview)  {
-    //         el.classList.add('inview');
-    //     } else {
-    //         el.classList.remove('inview');
-    //     }
-    // }
-
-    // const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
-
-    // const header = document.querySelector('.header')
-//     const _navAnimation = function (el, inview)  {
-//     if(inview)  {
-//         header.classList.remove('triggered');
-//     } else {
-//         header.classList.add('triggered');
-//     }
-//  }
-
-//     const so3 = new ScrollObserver('nav-trigger', _navAnimation, {once: false });
-
-    new MobileMenu;
+    
     new Main;
 });
 
 class Main {
     constructor() {
-        this.header = document.querySelector('.header')
+        this.header = document.querySelector('.header');
+        this.hero = new HeroSlider('.swiper');
         this._observers = [];
-        this._scrollInit();
+        this._init();
     }
 
+    _init() {
+        new MobileMenu;
+        this._scrollInit();
+    }
 
     _scrollInit() {
         this._observers.push(
             new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), { once: false }),
-            new ScrollObserver('.cover-slide', this._inviewAnimation)
+            new ScrollObserver('.swiper', this._toggleSlideAnimation.bind(this), { once: false }),
+            new ScrollObserver('.cover-slide', this._inviewAnimation),
+            new ScrollObserver('.tween-animate-title', this._textAnimation)
         )
         console.log(this._observers);
+    }
+     
+    _toggleSlideAnimation(el, inview)  {
+        if(inview)  {
+            this.hero.start();
+        } else {
+            this.hero.stop();
+        }
+     }
+
+    _textAnimation(el, isIntersecting)  {
+        if(isIntersecting)  {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
     }
 
     _navAnimation(el, inview)  {
